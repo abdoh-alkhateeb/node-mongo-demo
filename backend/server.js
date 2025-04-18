57137df1eb9fe1a8e4c1554a13193a04389dd5a7
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2025-04-18 21:59:14 +0200

    Fix minor bug with backend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
 
-    app.listen(process.env.PORT || 5000, () =>
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () =>
       console.log(`Server running at http://localhost:${port}`)
     );
   })
